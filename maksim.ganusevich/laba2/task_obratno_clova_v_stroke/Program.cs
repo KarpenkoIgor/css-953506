702e72613f1582ccdbd56f8c1c8d4e148ed5e113
953506 <62437619+953506@users.noreply.github.com>
2020-03-22 13:52:03 +0300

    Добавил лабу2

---
+++ maksim.ganusevich/laba2/task_obratno_clova_v_stroke/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0, lenght;
+            
+            StringBuilder myStringBuilder = new StringBuilder(Console.ReadLine(), 256); 
+            lenght = myStringBuilder.Length;
+            
+            for (int i = 0; i < lenght; i++)
+                if (myStringBuilder[i] == ' ')
+                    counter++;
+           
+            myStringBuilder.Append(' ');
+            lenght++;
+
+            for (int i = 0; counter!=0; i++)
+                if (myStringBuilder[i] == ' ')
+                {
+                    myStringBuilder.Insert(lenght+i, ' ');
+                    myStringBuilder.Remove(0, i+1);
+                    lenght -= (i+1);
+                    counter--;
+                    i = -1;
+                }
+                else
+                {
+                    myStringBuilder.Insert(lenght+i, myStringBuilder[i]);
+                }
+
+            Console.Write(myStringBuilder.ToString());
+
+            Console.Write($"\n{myStringBuilder.MaxCapacity} \t");
+        }
+    }
+}
---
+++ maksim.ganusevich/laba2/task_random_symbvol_iz_stroki/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str ="qwertyuioplkjhgfdsazxcvbnm123456qwertyuioplkjhgfdsazxcvbnm123456qwertyuioplkjhgfdsazxcvbnm123456qwertyuioplkjhgfdsazxcvbnm123456qwertyuioplkjhgfdsazxcvbnm123456qwertyuioplkjhgfdsazxcvbnm123456qwertyuioplkjhgfdsazxcvbnm123456qwertyuioplkjhgfdsazxcvbnm123456";
+            Console.Write($"\n{str.Length} \n");
+            
+            StringBuilder newStr = new StringBuilder(30);
+            Random random = new Random();
+
+            for (int i = 0; i < 30; i++)
+                newStr.Append(str[random.Next(256)]);
+
+            Console.Write(newStr.ToString());
+        }
+    }
+}
