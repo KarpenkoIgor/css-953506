90f61a860e337509daa43cd53e8feb54f692b1a2
MBindich <mbindich@gmail.com>
2020-03-23 15:35:21 +0300

    Add LAB 2 task 1

---
+++ bindich.mikhail/LAB 2/1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string: ");
+            string str = Console.ReadLine();
+
+            string[] strArr;
+            strArr = str.Split(' ');
+
+            for (int i = strArr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(strArr[i]);
+                Console.Write(' ');
+            }
+        }
+    }
+}
