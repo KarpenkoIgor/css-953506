c47e7e15d4cdd9b6bc8aa767249cb8d6bc0e9a7e
MBindich <mbindich@gmail.com>
2020-03-24 14:22:38 +0300

    Add LAB 2 task 2

---
+++ bindich.mikhail/LAB 2/2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _2_2
+{
+    class Program
+    {
+        static void symbolsCount(string date)
+        {
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int k = 0; k < date.Length; k++)
+                {
+                    int intK = Convert.ToInt32(date[k]) - 48;
+                    if (intK == i)
+                    {
+                        numbers[i]++;
+                    }
+                }
+
+                Console.WriteLine($"Amount of {i} - {numbers[i]}");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            string dateNow = DateTime.Now.ToString("MM/dd/yyyy HH:mm");
+            Console.WriteLine(dateNow);
+            symbolsCount(dateNow);
+
+            string dateNowUTC = DateTime.UtcNow.ToString("ddd, dd MMM yyy HH:mm:ss 'GMT'"); ;
+            Console.WriteLine(dateNowUTC);
+            symbolsCount(dateNowUTC);
+
+        }
+    }
+}
