f002624ce4b12731869039cf163aa9d4dc7e58b6
Rorojiko <62502281+Rorojiko@users.noreply.github.com>
2020-04-01 16:12:44 +0300

    css 2.2

---
+++ rajko.olga/ЛабораторнаяСШ2.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ЛабораторнаяСШ2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = Console.ReadLine();
+            string[] mstr = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string mystr in mstr)
+
+                 Array.Reverse(mstr);
+                 Console.WriteLine("reserved:");
+                 for (int i = 0; i < mstr.Length; i++)
+                 {
+                     Console.Write(mstr[i] + " ");
+                 }
+                
+        }
+    }
+}
+
