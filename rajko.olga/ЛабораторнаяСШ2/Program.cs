de52a17dae7841b307322ed8e4d38ef2c86143b3
Rorojiko <62502281+Rorojiko@users.noreply.github.com>
2020-04-01 16:11:06 +0300

    css 2.1

---
+++ rajko.olga/ЛабораторнаяСШ2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ЛабораторнаяСШ1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            DateTime  date1= DateTime.Now;
+            Console.WriteLine(date1.ToString());
+            Console.Write(date1.ToLongDateString());
+            Console.WriteLine(date1.ToLongTimeString());
+            string date2 = DateTime.Now.ToString();
+            int number = 0;
+            for (char num = '0'; num <= '9'; num++)
+            {
+                for (int chislo = 0; chislo < date2.Length; chislo++)
+                {
+                    if (date2[chislo] == num) number++;
+                }
+                Console.WriteLine("{0}:{1}", num, number);
+                number = 0;
+            }
+
+        }
+    }
+}
