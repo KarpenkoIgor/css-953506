4760786d06bbd155044187d429ca974a462b9651
afk121 <62508193+afk121@users.noreply.github.com>
2020-04-01 16:04:36 +0300

    Add files via upload

---
+++ LB2.2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        string str = "123.456";
+        int tochk = 0;
+        for(int i=0;i<str.Length;++i)
+        {
+            if ((str[i] < '0' || str[i] > '9') && str[i] != '.')
+                return;
+            if (str[i] == '.' && tochk == 0)
+                tochk = i;
+        }
+        double drob = 0;
+        for (int i = 0; i < str.Length; ++i)
+        {
+            if (str[i] == '.')
+            {
+                ++i;
+                ++tochk; 
+            }
+            drob += (str[i] - '0') * Math.Pow(10, tochk - i - 1);
+        }
+        Console.WriteLine(drob);
+        Console.ReadKey();
+    }
+}
