8b42dba50b9ad1ee80065a16dd4c60e1cd876991
953506 <62437619+953506@users.noreply.github.com>
2020-04-01 17:01:37 +0300

    Update data.cs

--- Barkovskaya.Elena/laba2/data.cs
+++ Barkovskaya.Elena/laba2/data.cs
@@ -26,7 +26,6 @@ namespace labr2
                 }
             }
             Console.WriteLine("Наименование цифры\tЕё количество\n\t0\t\t\t{0}\n\t1\t\t\t{1}\n\t2\t\t\t{2}\n\t3\t\t\t{3}\n\t4\t\t\t{4}\n\t5\t\t\t{5}\n\t6\t\t\t{6}\n\t7\t\t\t{7}\n\t8\t\t\t{8}\n\t9\t\t\t{9}\n\nТЫ ПИДОР\n\n", zero, one, two, three, four, five, six, seven, eight, nine);
-
             zero = 0; one = 0; two = 0; three = 0; four = 0; five = 0; six = 0; seven = 0; eight = 0; nine = 0;
             Console.WriteLine("\nВторой формат: " + now.ToString("G"));
             for (int i = 0; i < now.ToString("G").Length; i++)
