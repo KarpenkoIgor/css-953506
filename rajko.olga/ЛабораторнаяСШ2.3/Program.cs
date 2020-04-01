bf6e21cbf07d9ddbcb24ab5e2c6dab114ffd5d9d
Rorojiko <62502281+Rorojiko@users.noreply.github.com>
2020-04-01 16:16:51 +0300

    css 2.3

---
+++ rajko.olga/ЛабораторнаяСШ2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ЛабораторнаяСШ2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+            string[] letters ={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","v","r","s","t","u","v","w","x","y","z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "v", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "v", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "v", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "v", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "v", "r", "s", "t", "u", "v", "w", "x", "y", "z" ,"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "v", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "v", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "v", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "v", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "v", "r", "s", "t", "u", "v" };
+            Random rnd = new Random();
+            Console.Write("Random symbols: ");
+            for (int i = 0; i <= 30; i++)
+            {
+                int symbols = rnd.Next(letters.Length);
+                Console.Write(letters[symbols] + " ");
+            }
+
+        }
+    }
+}
