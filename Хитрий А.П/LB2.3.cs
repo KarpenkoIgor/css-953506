22ec008c98ada49d6ef4426770a7e0aee1046b00
afk121 <62508193+afk121@users.noreply.github.com>
2020-04-01 16:34:24 +0300

    Лаба 2,3

---
+++ Хитрий А.П/LB2.3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime dt = new DateTime (2000,1,1);
+            for (int i = 0; i < 12; ++i)
+            {
+                
+                Console.WriteLine(dt.AddMonths(i).ToString("MMMM", System.Globalization.CultureInfo.CreateSpecificCulture("fr-FR")));
+            }
+            Console.ReadKey();
+        }
+    }
+}
